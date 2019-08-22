314fd875ba78301d73956daa65479c8946838781
powerbarrusa <barr@covrigaru.com>
2019-08-22 14:35:25 -0600

    got stylesheet going. now for some state handling.

---
+++ App.js
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+import React from "react"
+import { StyleSheet, View, Text, TextInput, Button } from "react-native"
+
+const App = () => {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input} />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  }
+})
+
+export default App
